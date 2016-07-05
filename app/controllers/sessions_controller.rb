6864c5b2f2e3101f7d8ccc8906b593a9995c7e85
Cess <cessmbuguar@gmail.com>
2016-07-05 22:31:31 +0300

    login, create, and current user method for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
 user = User.find_by(username: params[:session][:username])
 if user && user.authenticated(params[:session][:password])
+  log_in user
+  redirect_to user
 else
   flash[:danger] = "Check your user/password combination"
   render 'new'
