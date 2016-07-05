c053495335cd53aca18d4c777065ddfa7675fbe1
Cess <cessmbuguar@gmail.com>
2016-07-05 22:15:19 +0300

    create a login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+user = User.find_by(username: params[:session][:username])
+if user && user.authenticated(params[:session][:password])
+else
+  flash[:danger] = "Check your user/password combination"
+  render 'new'
+end
+  end
+
+  def destroy
+
+  end
+
 end
