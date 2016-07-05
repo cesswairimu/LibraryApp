b9ad76bafbaf5b2164872190c47a4fb29c5f41d1
Cess <cessmbuguar@gmail.com>
2016-07-05 12:36:51 +0300

    generate pages home,support and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+ root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+
+  get 'support' => 'static_pages#support'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
