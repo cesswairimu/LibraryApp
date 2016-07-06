dff1c2c3e1234084f40ad565a0bb913952c841d8
Cess <cessmbuguar@gmail.com>
2016-07-06 21:33:51 +0300

    debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-user = User.find_by(username: params[:session][:username])
-if user && user.authenticate(params[:session][:password])
-  log_in user
-  redirect_to user
-else
-  flash[:danger] = "Check your user/password combination"
-  render 'new'
-end
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = "Check your user/password combination"
+      render 'new'
+    end
   end
 
   def destroy
