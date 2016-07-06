f59d7d02a0a8aa1254f7899f0754be2c9a565d90
Cess <cessmbuguar@gmail.com>
2016-07-06 21:14:19 +0300

    solving bad URI prob

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
 user = User.find_by(username: params[:session][:username])
-if user && user.authenticated(params[:session][:password])
+if user && user.authenticate(params[:session][:password])
   log_in user
   redirect_to user
 else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationControllee
+class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:edit, :update]
 
