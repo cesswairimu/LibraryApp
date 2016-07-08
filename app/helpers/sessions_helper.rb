525992cdf8c759fad5d8716f12a08b1c54ecb5a1
Cess <cessmbuguar@gmail.com>
2016-07-08 09:34:49 +0300

    commenting on methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,19 @@
 module SessionsHelper
+  # sets the session for the usser
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  #defning the current user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # returns true if user is looged in
   def logged_in
     !current_user.nil?
   end
-
+  #deletes the session created above
 def log_out
   session.delete(:user_id)
  @current_user = nil
