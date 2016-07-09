7fe54bb1593b238cac4815efe654e1a60c072009
Cess <cessmbuguar@gmail.com>
2016-07-09 15:13:24 +0300

    add index uniqueness in email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+before_save { self.username = username.downcase }
   VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, length: { maximum: 30 }, 
-            uniqueness: { case_sensitive: false }, format: { with: VALID_REGEX }
+            uniqueness: true, format: { with: VALID_REGEX }
   has_secure_password
   validates :password, length: { minimum: 6 }
   
