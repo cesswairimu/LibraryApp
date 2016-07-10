32091e9e31c96f4f67b2cc5b59a0f5ac947f6957
Cess <cessmbuguar@gmail.com>
2016-07-10 10:14:31 +0300

    tests for logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      #login user after sign up
       log_in @user
       flash[:success] = "Welcome to our Library"
       redirect_to @user
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,15 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+
+  fixtures :all
+  
+
+  #testing the behaviour if a user is logged in after signup 
+  def is_logged_in?
+    !session[:user_id].nil?
+
+  end
+
 end
