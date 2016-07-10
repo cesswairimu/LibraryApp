9601253b44d9146982f38b85e973200f9826d82b
Cess <cessmbuguar@gmail.com>
2016-07-10 12:29:03 +0300

    remove space between forget and (user)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       #handling submission of the remember token
-      params[:session][:remember_me] == '1' ? remember(user) : forget (user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = "Check your user/password combination"
