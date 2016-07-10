96bb5e3b6aeb1be4c5d239fd2f7ca3b23f1bf27f
Cess <cessmbuguar@gmail.com>
2016-07-10 11:01:05 +0300

    current user for persistence session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  #defning the current user
+  #current_user for persistence
   def current_user
+    if (user_id = session[:user_id])
     @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:rem_token])
+        log_in user
+        @current_user = user
+  end
+    end
   end
 
   # returns true if user is looged in
@@ -22,7 +30,8 @@ end
 #creating a user remember cookie
 def remember(user)
 user.remember
-cookies[:user.id] = { value:user_id, expires: 7.days.from_now.utc }
+#decrypts the user id cookie
+cookies.signed[:user.id] = { value:user_id, expires: 7.days.from_now.utc }
 cookies[:rem_token] = {value: rem_token, expires: 7.days.from_now.utc }
 end
 end
