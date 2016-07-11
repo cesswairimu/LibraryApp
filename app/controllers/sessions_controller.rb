5c3a435e31f3d88a7ea59f1920989e8691359e72
Cess <cessmbuguar@gmail.com>
2016-07-11 23:19:55 +0300

    prevent unactivated users from loggin in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
+      if user.activated?
       log_in user
       #handling submission of the remember token
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:danger] = "Check your user/password combination"
+      message = "Account not activated"
+      message += "Check your email  for  activation"
+      flash.now[:warning] = message
+      redirect_to root_url
+    end
+    else
+      flash.now[:danger] = "Incorrect username/password combination"
       render 'new'
     end
   end
