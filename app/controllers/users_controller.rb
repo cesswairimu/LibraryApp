1f6873a1fd2fe008b205224c145880561341e874
Cess <cessmbuguar@gmail.com>
2016-07-11 21:46:38 +0300

    adding activation mailer during signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Check your email to activate account"
       #login user after sign up
-      log_in @user
-      flash[:success] = "Welcome to our Library"
-      redirect_to @user
+     # log_in @user
+      #flash[:success] = "Welcome to our Library"
+      redirect_to root_url
     else
       render 'new'
     end
