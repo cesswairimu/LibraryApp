7bdf67dd4ac44b50bad10897cd0a84292153c9bb
Cess <cessmbuguar@gmail.com>
2016-07-11 22:52:44 +0300

    testing the authenticated generalized message

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,49 +3,48 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Ceciliah", username: "@Cessy", email: "cessmkali@wew.com", 
-                    password: "terrence", password_confirmation: "terrence")
+                     password: "terrence", password_confirmation: "terrence")
   end
 
 
-test " username should be unique" do
-  duplicate_user = @user.dup
-  @user.save
-  assert_not duplicate_user.valid?
-end
+  test " username should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
-test "username should be present" do
-  @user.username = ""
-  assert_not @user.valid?
-end
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
 
-test "name should be present" do
-  @user.name = ""
-  assert_not @user.valid?
-end
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
 
-test "email should be present" do
-  @user.email = ""
-  assert_not @user.valid?
-end
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 
 
-test "email should length not big" do
-  @user.email = "a" * 50 + "@example.com"
-  assert_not @user.valid?
-end
+  test "email should length not big" do
+    @user.email = "a" * 50 + "@example.com"
+    assert_not @user.valid?
+  end
 
-test "email addresses should be unique" do
-  duplicate_user = @user.dup
-  @user.save
-  assert_not duplicate_user.valid?
-end
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
-test "return false authenticated? for a user with nil digest" do
-assert_not @user.authenticated?('')
-end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
 end
