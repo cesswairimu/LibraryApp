cc7c890d29bc2f515911529e518b0c76d40fba2b
Cess <cessmbuguar@gmail.com>
2016-07-12 23:01:46 +0300

    add create action for the password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,4 +4,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def create
+    @user = User.find_by(username: params[:password_reset][:username])
+
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash[:danger] = "No such username"
+      render 'new'
+    end
+  end
 end
