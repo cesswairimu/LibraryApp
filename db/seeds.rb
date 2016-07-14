c051665218e450139d4dafba1f94b49ce67dcce7
Cess <cessmbuguar@gmail.com>
2016-07-14 14:40:35 +0300

    added a new user and set the other one administrative rights

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,16 @@ User.create!( name: "cess",
              email: "cess@gmail.com",
              password: "terrence",
              password_confirmation: "terrence",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
+User.create!( name: "lawrence",
+             username: "lawrence",
+             email: "lawrence@gmail.com",
+             password: "lawrence",
+             password_confirmation: "lawrence",
              activated: true,
              activated_at: Time.zone.now)
 
