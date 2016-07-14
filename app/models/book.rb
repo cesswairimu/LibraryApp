68da28de83406333adc647b5f0780165de973ac0
Cess <cessmbuguar@gmail.com>
2016-07-14 23:01:00 +0300

    add input for the user fields to be present

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :publisher, presence: true
+  validates :quantity, presence: true
+  validates :category, presence: true
 end
