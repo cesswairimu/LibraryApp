ac4aa8f881ef4a612ee6be49165866eac166142e
Cess <cessmbuguar@gmail.com>
2016-07-14 14:25:01 +0300

    changed user to @user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -47,7 +47,7 @@ end
   end
 
   def valid_user
-  unless @user && user.activated? &&
+  unless @user && @user.activated? &&
     @user.authenticated?(:reset, params[:id])
     redirect_to root_url
   end
