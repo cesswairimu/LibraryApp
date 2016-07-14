f2937307551a4135eb1697def38bab95d41dcb80
Cess <cessmbuguar@gmail.com>
2016-07-14 23:12:39 +0300

    tests for book validations

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = Book.new(title: "Whale Rider", category: "Literature",
+                    publisher: "Longhorn", author: "Cess Mbugua", quantity: 7 )
+  end
+
+  test "title should be present" do
+    @book.title = ""
+    assert_not @book.valid?
+  end
+ test "quantity should be present" do
+    @book.quantity = ""
+    assert_not @book.valid?
+  end
+
+ test "category should be present" do
+    @book.category = ""
+    assert_not @book.valid?
+  end
+ test "publisher should be present" do
+    @book.publisher = ""
+    assert_not @book.valid?
+  end
+ test "author should be present" do
+    @book.author =  ""
+    assert_not @book.valid?
+  end
+
 end
