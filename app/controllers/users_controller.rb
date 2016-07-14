ef7aa105277292303dd716bdc637fefa938acb56
Cess <cessmbuguar@gmail.com>
2016-07-14 14:56:56 +0300

    add action for admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:edit, :update]
-
- def new
+  before_action :admin, only: [:index]
+  def new
     @user = User.new
   end
 
@@ -10,24 +10,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
- # def index
-  #  @users = User.all
-  #end
-  
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
       flash[:info] = "Check your email to activate account"
       #login user after sign up
-     # log_in @user
+      # log_in @user
       #flash[:success] = "Welcome to our Library"
       redirect_to root_url
     else
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
@@ -42,13 +42,17 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
   def logged_in_user
     unless logged_in
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+#confirm a user is an admin
+  def admin
+    redirect_to root_url unless current_user.admin?
   end
 
 
