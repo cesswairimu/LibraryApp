a8f3805fa5126fc2fbd1506c3087c26ac401102b
Cess <cessmbuguar@gmail.com>
2016-07-15 12:13:30 +0300

    action for add a new book and defining the book params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
 class BooksController < ApplicationController
-  def add
+
+  def new
     @book = Book.new
+  end
+
+  def add
+    @book = Book.new(book_params)
+    if @user.save
+      flash[:info] = "Book added successfully"
+      redirect_to new_url
+    else
+      render 'new'
+    end
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, 
+                                 :category, :author,
+                                :publisher, :quantity)
   end
 end
