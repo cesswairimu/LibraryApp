1f1ec343d2fa1d2924987c71b1221a80819d31bf
Cess <cessmbuguar@gmail.com>
2016-07-17 13:42:49 +0300

    logged_in user to be used by all the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,11 @@ class ApplicationController < ActionController::Base
   def admin
     current_user.admin?
   end
-
+  #require a user to be logged in
+ def logged_in_user
+    unless logged_in
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+ end  
 end
