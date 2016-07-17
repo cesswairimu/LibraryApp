5d00e400877f691c8b34ceeab3a9d03643e5a2e4
Cess <cessmbuguar@gmail.com>
2016-07-17 13:43:39 +0300

    remove the logged_in user method from private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,13 +48,6 @@ class UsersController < ApplicationController
 
 
   private
-  def logged_in_user
-    unless logged_in
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-#confirm a user is an admin
   
   def user_params
     params.require(:user).permit(:name, :username, :email, :password,
