0ae2cb5bc809d44318defd63e993871fe0d099de
Cess <cessmbuguar@gmail.com>
2016-07-17 12:17:31 +0300

    test for the new book action

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,13 +2,9 @@ require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get book_path
+    get new_book_path
     assert_response :success
   end
 
-  test "should get add" do
-    get book_url 
-    assert_response :success
-  end
 
 end
