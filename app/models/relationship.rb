3be71f34d06c59fd0d22e88b986b79a812ef5130
Cess <cessmbuguar@gmail.com>
2016-07-20 20:43:59 +0300

    delete relationship model and controller

--- app/models/relationship.rb
+++
@@ -1,7 +0,0 @@
-class Relationship < ApplicationRecord
-  belongs_to :borrower, class_name: "User"
-  belongs_to :borrowed, class_name: "Book"
-  validates :borrower_id,  presence: true
-  validates :borrowed_id,  presence: true
-
-end
