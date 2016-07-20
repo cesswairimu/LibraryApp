87d776e8ca0aeb88aad10ef0b04c6ac7cb236718
Cess <cessmbuguar@gmail.com>
2016-07-20 15:34:50 +0300

    relationship model for borrowers and borrowed books

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+  belongs_to :borrower, class_name: "User"
+  belongs_to :borrowed, class_name: "Book"
+  validates :borrower_id,  presence: true
+  validates :borrowed_id,  presence: true
+
 end
