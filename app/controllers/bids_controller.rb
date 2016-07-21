c50682f856c4204ad73b25223fcee176bb76bc34
Cess <cessmbuguar@gmail.com>
2016-07-21 11:44:49 +0300

    index, show and release method for bids

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -5,12 +5,26 @@ class BidsController < ApplicationController
   def create
     @bid = Bid.new(bid_params)
     if @bid.save
-      flash[:success] = "Ceeeeeeeeeeeeeessssssssssssssssssssss!!!!!!!!!!!!!!!!!!!!"
+      flash[:success] = "Ceeeeeeeeeeeeeessssssssssssssssssssss!!!!!!!!!!!!!!!!!!!! "
       redirect_to books_path
     else
       flash[:danger] = "Problem requesting for a book"
       redirect_to books_path
     end
+  end
+  def index
+    @bids = Bid.all
+  end
+
+  def show
+    @bid = Bid.find(bid_params)
+  end
+
+  def release
+    @bid = Bid.find(params[:bid_id])
+    @bid.update_attribute(:status, params[:status])
+    flash[:success] = "You have checked out this book"
+    redirect_to bids_path
   end
 
 
