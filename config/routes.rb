03759ae9c8dd3063ee3c043b11c2f0a6d0b86d99
Cess <cessmbuguar@gmail.com>
2016-07-21 21:10:04 +0300

    use resources only for books routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,17 @@
 Rails.application.routes.draw do
-
-
   root 'static_pages#home'
-
   get 'about' => 'static_pages#about'
-
   get 'support' => 'static_pages#support'
-
   get 'signup' => 'users#new'
-
   get 'login' => 'sessions#new'
   get 'index' => 'users#index'
-
   post 'login' => 'sessions#create'
-
   delete 'logout' => 'sessions#destroy'
-  get 'book' => 'books#show'
-  post 'release' => 'bids#release'
+  get 'release' => 'bids#release'
 
-  resources :users 
+  resources :users
   resources :account_activations, only:[:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
-  resources :books 
+  resources :books
   resources :bids
 end
