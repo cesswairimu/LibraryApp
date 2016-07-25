e18dfa9d44fbb45caef005cb050c7c80e8356955
Cess <cessmbuguar@gmail.com>
2016-07-25 21:51:03 +0300

    include module for bid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  include BidsHelper
 
 
   #require a user to be logged in
