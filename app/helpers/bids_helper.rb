f9a9723fb70802b159c395bfa660d6af3d59db31
Cess <cessmbuguar@gmail.com>
2016-07-26 18:03:39 +0300

    add distance in time method for due date

--- app/helpers/bids_helper.rb
+++ app/helpers/bids_helper.rb
@@ -1,6 +1,9 @@
 module BidsHelper
   def days_remaining(bid)
-    bid.due_date = Time.now + 20.days
+    #if bid.due_date.future?
+      unless bid.due_date.nil?
+        distance_of_time_in_words( Time.zone.now, bid.due_date )
+      end
   end
 
 end
