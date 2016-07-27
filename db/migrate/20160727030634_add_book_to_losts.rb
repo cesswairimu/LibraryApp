5247aa2593b8769d8a8a7e09f1efd0430244022d
Cess <cessmbuguar@gmail.com>
2016-07-27 09:20:43 +0300

    solving database mess ups

--- db/migrate/20160727030634_add_book_to_losts.rb
+++
@@ -1,5 +0,0 @@
-class AddBookToLosts < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :losts, :book, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727030634) do
+ActiveRecord::Schema.define(version: 20160722052828) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,19 +33,6 @@ ActiveRecord::Schema.define(version: 20160727030634) do
     t.string   "publisher"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "losts", force: :cascade do |t|
-    t.integer  "bid_id"
-    t.string   "penalty"
-    t.string   "deadline"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.integer  "book_id"
-    t.index ["bid_id"], name: "index_losts_on_bid_id"
-    t.index ["book_id"], name: "index_losts_on_book_id"
-    t.index ["user_id"], name: "index_losts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
