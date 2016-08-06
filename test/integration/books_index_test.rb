53530ccf43b0fd399d35d1b6c3ac4b5bccb96349
Cess <cessmbuguar@gmail.com>
2016-08-06 20:57:20 +0300

    test books index

--- test/integration/books_index_test.rb
+++ test/integration/books_index_test.rb
@@ -10,4 +10,22 @@ class BooksIndexTest < ActionDispatch::IntegrationTest
     get books_path
     assert_template 'books/index'
   end
+  test "should search by title" do
+    skip
+    assert login_as(@user)
+    get books_path 
+    get params: {
+      search: {
+      }
+    }
+  end
+test "should request a book" do
+  assert login_as(@user)
+  get books_path
+  assert_template 'books/index'
+  get new_bid_path
+  post params: { 
+  bid: { book_id: 12 
+  }}
+end
 end
