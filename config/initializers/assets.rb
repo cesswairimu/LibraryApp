5281f14cda06171b682d9cbeaaa214428e377c53
Cess <cessmbuguar@gmail.com>
2018-06-20 12:55:55 +0300

    initializing compass

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+require 'compass'
+Sass.load_paths << Compass::Frameworks['compass'].stylesheets_directory
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
