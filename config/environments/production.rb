231757d0cf3232ef9ea8970ba04cfcb120ec0744
Cess <cessmbuguar@gmail.com>
2018-10-14 16:15:50 +0300

    config mailers for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'quiet-earth-77527.herokuapp.com'
   config.action_mailer.default_url_options = { host: host  }
+  ActionMailer::Base.smtp_settings = {
+    :address         => 'smtp.sendgrid.net',
+    :port            => '587',
+    :authentication  => :plain,
+    :user_name       => ENV['SENDGRID_USERNAME'],
+    :password        => ENV['SENDGRID_PASSWORD'],
+    :domain          => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
